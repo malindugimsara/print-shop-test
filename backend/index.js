7e06e3e770692a34d05c74eb7f4159999db5f052
Malindu <gimsaramalindu2002@gmail.com>
2025-12-25 01:02:30 +0530

    back end  file updated

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,8 @@ app.use("/api/job", jobRouter);
 app.use("/api/jobcustomer", jobCustomerRouter);
 app.use("/api/customer", customerRouter);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+// app.listen(3000, () => {
+//   console.log('Server is running on port 3000');
+// });
 
+export default app;
